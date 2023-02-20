383ad93cc2eb3d9c5e2cf4f9163e637eae9068b0
Veniamin Shemyakin <122688291+VenyOk@users.noreply.github.com>
2023-02-20 21:19:28 +0300

    Create VisMealy.go

---
+++ VisMealy/VisMealy.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+const N = 1000
+const M = 1000
+
+func main() {
+	var n, m, q0 int
+	fmt.Scan(&n)
+	fmt.Scan(&m)
+	fmt.Scan(&q0)
+	arr := [N][M]int{}
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			fmt.Scan(&arr[i][j])
+		}
+	}
+	arr2 := [N][M]string{}
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			fmt.Scan(&arr2[i][j])
+		}
+	}
+	fmt.Println("digraph {")
+	fmt.Println("\trankdir = LR")
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			fmt.Printf("\t%d -> %d [label = \"%c(%s)\"]", i, arr[i][j], 97+j, arr2[i][j])
+			fmt.Println()
+		}
+	}
+	fmt.Println("}")
+}
