60fff745c863152aad1f4ec4c4b8c36b715f0db3
Veniamin Shemyakin <122688291+VenyOk@users.noreply.github.com>
2023-06-22 21:39:37 +0300

    Update econom.go

--- econom/econom.go
+++ econom/econom.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "bufio"
+    "os"
+)
 
 func in_array(arr []string, s string) (result bool) {
 	result = false
@@ -12,16 +16,17 @@ func in_array(arr []string, s string) (result bool) {
 	}
 	return
 }
+
 func econom(s string) (result int) {
 	var used_oper []string
 	var braces []int
 	result = 0
 	for i, symb := range s {
 		if string(symb) == "(" {
-			braces = append(braces, i) // добавление индекса открывающейся скобки
+			braces = append(braces, i) 
 		} else if string(symb) == ")" {
 			if !in_array(used_oper, s[braces[len(braces)-1]:i]) {
-				used_oper = append(used_oper, s[braces[len(braces)-1]:i]) // добавление "внутренности" между скобками
+				used_oper = append(used_oper, s[braces[len(braces)-1]:i])
 			}
 			braces = braces[:len(braces)-1]
 		}
@@ -29,7 +34,10 @@ func econom(s string) (result int) {
 	result = len(used_oper)
 	return
 }
+
 func main() {
-	fmt.Println(econom("(#($(#xy)($(#ab)(#ab)))(@z($(#ab)(#ab))))"))
-	fmt.Println(econom("(#($ab)($ab))"))
+    scanner := bufio.NewScanner(os.Stdin)
+    scanner.Scan()
+    s := scanner.Text()
+    fmt.Println(econom(s))
 }
