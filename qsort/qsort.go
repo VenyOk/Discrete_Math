b9810bdf42980d75ab96d4f4c8c672532b211304
Veniamin Shemyakin <122688291+VenyOk@users.noreply.github.com>
2023-06-22 21:36:27 +0300

    Update qsort.go

--- qsort/qsort.go
+++ qsort/qsort.go
@@ -1,20 +1,6 @@
 package main
 
-import "fmt"
-
-var arr = []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1}
-
-// Код полностью взят из презентации 2 модуля Алгоритмов и структур данных
-
-func swap(i, j int) {
-	arr[i], arr[j] = arr[j], arr[i]
-}
-
-func less(i, j int) (result bool) {
-	result = arr[i] < arr[j]
-	return
-}
-func partition(l, r int) (i int) {
+func partition(l int, r int, less func(i, j int) bool, swap func(i, j int)) (i int) {
 	i = l
 	j := l
 	for j < r {
@@ -27,18 +13,19 @@ func partition(l, r int) (i int) {
 	swap(i, r)
 	return
 }
-func qsortrec(l, r int) {
+
+func qsortrec(l int, r int, less func(i, j int) bool, swap func(i, j int)) {
 	if l < r {
-		q := partition(l, r)
-		qsortrec(l, q-1)
-		qsortrec(q+1, r)
+		q := partition(l, r, less, swap)
+		qsortrec(l, q-1, less, swap)
+		qsortrec(q+1, r, less, swap)
 
 	}
 }
+
 func qsort(n int, less func(i, j int) bool, swap func(i, j int)) {
-	qsortrec(0, n-1)
+	qsortrec(0, n-1, less, swap)
 }
+
 func main() {
-	qsort(10, less, swap)
-	fmt.Println(arr)
 }
